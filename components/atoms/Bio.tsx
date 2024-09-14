df3e1c334704654783ae35098c96816404bf4c13
danielder10 <dder10@my.bcit.ca>
2024-09-13 20:35:37 -0700

    testing

--- components/atoms/Bio.tsx
+++ components/atoms/Bio.tsx
@@ -2,7 +2,7 @@ import { Text, View } from "react-native";
 
 export default function Bio({ bioText }) {
   return (
-    <View style={{ alignItems: "center", border:"1px solid", width:"50%", marginTop: 10}}>
+    <View style={{ alignItems: "center", border: "1px solid", width:"50%", marginTop: 10}}>
       <Text style={{ color: "salmon", fontSize: 18, marginBottom: 10, marginTop: 10, width: "90%", alignItems: "center"}}>
         {bioText}
       </Text>
